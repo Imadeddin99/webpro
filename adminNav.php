2958eaaca29b3a4b661dc2d6a7474650aa658c7b
Imadsalama99 <i.salama@stu.najah.edu>
2020-04-03 22:26:12 +0300

    eighth Commit

--- adminNav.php
+++ adminNav.php
@@ -1,23 +1,112 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Admin Control Panel</title>
-    <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+<!--<html lang="en">-->
+<!--<head>-->
+<!--    <meta charset="UTF-8">-->
+<!--    <title>Admin Control Panel</title>-->
+<!--    <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>-->
+<!---->
+<!--    <script src="https://kit.fontawesome.com/97db899fc1.js" crossorigin="anonymous"></script>-->
+<!--    <link href="http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">-->
+<!--    <link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">-->
+<!--    <script src="http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>-->
+<!--    <script src="nav/jquery.responsive-collapse.js" type="text/javascript"></script>-->
+<!-- <script src="nav/bootstrap.responsive-collapse.js" type="text/javascript"></script>-->
+<!--    <link href="nav/jquery.responsive-collapse.css" rel="stylesheet">-->
+<!--    <style>-->
+<!--        body { background-color: #fafafa; font-family:'Roboto';}-->
+<!--        h1 { margin:70px auto; text-align:center;}-->
+<!--    </style>-->
+<!--</head>-->
+<!--<body>-->
+<!--<div class="navbar navbar-inverse navbar-fixed-top" style="background-color: #34495e;position: initial;">-->
+<!--    <div class="container">-->
+<!--        <div class="navbar-header">-->
+<!--            <a class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">-->
+<!--                <span class="icon-bar"></span>-->
+<!--                <span class="icon-bar"></span>-->
+<!--                <span class="icon-bar"></span>-->
+<!--            </a>-->
+<!--        </div>-->
+<!--        <img src="eng%202.png" style="width: 25% ; margin-left:-9%;" >-->
+<!---->
+<!--        <div class="navbar-collapse collapse navbar-right">-->
+<!--            <ul class="nav navbar-nav">-->
+<!--                <li><a href="mainPage.php"> Training Page</a></li>-->
+<!--                <li><a href="employeePage.php">Employee page</a></li>-->
+<!--                <li><a href="formsPage.php">Forms Page</a></li>-->
+<!--                <li><a href="filePage.php">Files Page</a></li>-->
+<!--                <li><a href="calenderPage.php">Calender Page</a></li>-->
+<!---->
+<!---->
+<!--                <li class="dropdown">-->
+<!--                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> User <b class="caret"></b></a>-->
+<!--                    <ul class="dropdown-menu">-->
+<!--                        <li><a href="userp.php">Profile</a></li>-->
+<!--                        <li class="divider"></li>-->
+<!--                        <li><a href="index.php" name="logout">Logout</a></li>-->
+<!--                    </ul>-->
+<!--                </li>-->
+<!--            </ul>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--</div>-->
+<!---->
+<!---->
+<!--<script src="http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>-->
+<!--<script src="nav/jquery.responsive-collapse.js"></script>-->
+<!---->
+<!--<script type="text/javascript">-->
+<!--    $(window).load(function() {-->
+<!--        $('ul.navbar-nav').responsiveCollapse();-->
+<!--    });-->
+<!--</script>-->
+<!--<!--<script type="text/javascript">-->-->
+<!--<!---->-->
+<!--<!--    var _gaq = _gaq || [];-->-->
+<!--<!--    _gaq.push(['_setAccount', 'UA-36251023-1']);-->-->
+<!--<!--    _gaq.push(['_setDomainName', 'jqueryscript.net']);-->-->
+<!--<!--    _gaq.push(['_trackPageview']);-->-->
+<!--<!---->-->
+<!--<!--    (function() {-->-->
+<!--<!--        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;-->-->
+<!--<!--        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';-->-->
+<!--<!--        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);-->-->
+<!--<!--    })();-->-->
+<!--<!---->-->
+<!--<!--</script>-->-->
+<?php
+//
+//if (isset($_POST['logout'])){
+//    session_destroy();
+//
+//}
+//
+//?>
+<!---->
+<!---->
+<!---->
+<!---->
+<!--</body>-->
+<!--</html>-->
 
-    <script src="https://kit.fontawesome.com/97db899fc1.js" crossorigin="anonymous"></script>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admin NavBar</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap -->
     <link href="http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
     <link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-    <script src="http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script src="nav/jquery.responsive-collapse.js" type="text/javascript"></script>
- <script src="nav/bootstrap.responsive-collapse.js" type="text/javascript"></script>
+
     <link href="nav/jquery.responsive-collapse.css" rel="stylesheet">
     <style>
-        body { background-color: #fafafa; font-family:'Roboto';}
+        body { background-color:#fafafa; font-family:'Roboto';}
         h1 { margin:70px auto; text-align:center;}
     </style>
 </head>
+
 <body>
-<div class="navbar navbar-inverse navbar-fixed-top" style="background-color: #34495e;position: initial;">
+<div class="navbar navbar-inverse navbar-fixed-top" style="background-color: #34495e">
     <div class="container">
         <div class="navbar-header">
             <a class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
@@ -25,32 +114,41 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </a>
+            <a href="#" class="navbar-brand" style="color: white">Scientific Centers An-Najah National University</a>
         </div>
-        <img src="eng%202.png" style="width: 25% ; margin-left:-9%;" >
-
         <div class="navbar-collapse collapse navbar-right">
             <ul class="nav navbar-nav">
-                <li><a href="mainPage.php"> Training Page</a></li>
-                <li><a href="employeePage.php">Employee page</a></li>
-                <li><a href="formsPage.php">Forms Page</a></li>
-                <li><a href="filePage.php">Files Page</a></li>
-                <li><a href="calenderPage.php">Calender Page</a></li>
+                <li><a href="#"> Training Page</a></li>
+                <li><a href="filePage.php"> Files Page</a></li>
 
+                <li><a href="formsPage.php"> Forms Page </a></li>
+                <li><a href="employeePage.php"> Employees Page</a></li>
+                <li><a href="calenderPage.php"> Calender Page</a></li>
 
                 <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> User <b class="caret"></b></a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"> User <b class="caret"></b></a>
                     <ul class="dropdown-menu">
                         <li><a href="userp.php">Profile</a></li>
                         <li class="divider"></li>
-                        <li><a href="index.php" name="logout">Logout</a></li>
+                        <li><a href="index.php">Logout</a></li>
                     </ul>
                 </li>
             </ul>
         </div>
     </div>
 </div>
-
-
+<div class="jquery-script-ads" align="center"><script type="text/javascript"><!--
+        google_ad_client = "ca-pub-2783044520727903";
+        /* jQuery_demo */
+        google_ad_slot = "2780937993";
+        google_ad_width = 728;
+        google_ad_height = 90;
+        //-->
+    </script>
+    <script type="text/javascript"
+            src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+    </script></div>
+<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
 <script src="http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script src="nav/jquery.responsive-collapse.js"></script>
 
@@ -59,31 +157,56 @@
         $('ul.navbar-nav').responsiveCollapse();
     });
 </script>
-<!--<script type="text/javascript">-->
-<!---->
-<!--    var _gaq = _gaq || [];-->
-<!--    _gaq.push(['_setAccount', 'UA-36251023-1']);-->
-<!--    _gaq.push(['_setDomainName', 'jqueryscript.net']);-->
-<!--    _gaq.push(['_trackPageview']);-->
-<!---->
-<!--    (function() {-->
-<!--        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;-->
-<!--        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';-->
-<!--        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);-->
-<!--    })();-->
-<!---->
-<!--</script>-->
-<?php
+<script type="text/javascript">
 
-if (isset($_POST['logout'])){
-    session_destroy();
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-36251023-1']);
+    _gaq.push(['_setDomainName', 'jqueryscript.net']);
+    _gaq.push(['_trackPageview']);
 
-}
+    (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
 
-?>
-
-
-
-
+</script>
 </body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
